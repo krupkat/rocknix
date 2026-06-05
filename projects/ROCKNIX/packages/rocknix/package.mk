85f9c1e886878bc1f1d870cd5c385a5ae1f94421
Tomas Krupka <tomas.krupka@goodvisionlive.com>
2026-06-05 14:34:45 +0200

    disable samba

--- projects/ROCKNIX/packages/rocknix/package.mk
+++ projects/ROCKNIX/packages/rocknix/package.mk
@@ -84,7 +84,6 @@ EOF
   BUILD_BRANCH="$(git branch --show-current)"
   if [ ! "${BUILD_BRANCH}" = "main" ]
   then
-    sed -i "s#samba.enabled=0#samba.enabled=1#g" ${INSTALL}/usr/config/system/configs/system.cfg
     sed -i "s#ssh.enabled=0#ssh.enabled=1#g" ${INSTALL}/usr/config/system/configs/system.cfg
     sed -i "s#wifi.enabled=0#wifi.enabled=1#g" ${INSTALL}/usr/config/system/configs/system.cfg
     sed -i "s#system.loglevel=none#system.loglevel=verbose#g" ${INSTALL}/usr/config/system/configs/system.cfg
